b49ec68593743ec69532d0d149de2cf4c6c6fdbc
LinWei <ndivhy4203@163.com>
2026-08-08 16:36:40 +0800

    修正import笔记:区分包、类、方法

---
+++ Java语法笔记/02-import拆解.md
@@ -0,0 +1,18 @@
+import是个什么的？
+
+是告诉编译器要用哪个类
+
+
+
+为什么没有它编译器就报错？
+
+因为编译器找不到下列代码使用的类，认不出它
+
+
+
+Java.utli.ArrayList里的java.utli是什么？
+
+java包
+
+如 java.util(包）--ArrayList(类）--add()(方法)
+
---
+++ Java语法笔记/TestImport.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class TestImport{
+	public static void main(String[] args){
+		ArrayList<String>list=new ArrayList<>();
+		list.add("测试");
+		System.out.println(list.get(0));
+	}
+}
